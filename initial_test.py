982db8385b7f66a36feb71c09c80e2de4971f0df
MiguelMonteiro98 <36896593+MiguelMonteiro98@users.noreply.github.com>
2021-08-26 17:18:45 +0100

    models

--- initial_test.py
+++ initial_test.py
@@ -1,16 +1,11 @@
 import spacy
 
-nlp = spacy.load('pt_core_news_md')
-
-
 def get_labels(file):
     print(file)
-    data = open(file, "r").readlines()
+    data = open(file, "r", encoding='utf-8').readlines()
 
     data = [d for d in data if len(d.split("\t")) == 2]
     
-
-    texts = [text.split("\t")[0] for text in data]
     labels = [text.split("\t")[1].replace("\n", "") for text in data]
     labels = set(labels) 
     print(labels, '\n')
--- training.py
+++ training.py
@@ -1,28 +1,40 @@
 import os
+import sys
+
+def run(model_name, gpu = False, train = False):
+
+    if gpu:
+        gpu_cmd = "--gpu-id 0"
+    else:
+        gpu_cmd = "--gpu-id -1"
+
+    config_cmd = "python -m spacy init fill-config base_config.cfg config.cfg"
+    spacy_path = "spacy_data/"
+
+    
+    dev_data = "corpus_First_HAREM.spacy"
+    train_data = "corpus_paramopama+second_harem.spacy"
+
+    data_path = spacy_path + train_data
+    dev_path = spacy_path + dev_data
+
+    train_path_changer = "--paths.train "+data_path
+
+    dev_path_changer = "--paths.dev "+dev_path
+
+    print("training data: " ,train_path_changer)
+    print("evaluation data: " ,dev_path_changer)
+    verbose = "--verbose"
 
 
-config_cmd = "python -m spacy init fill-config base_config.cfg config.cfg"
-spacy_path = "spacy_data/"
-train_data = "corpus_First_HAREM.spacy"
-dev_data = "corpus_paramopama+second_harem.spacy"
 
-data_path = spacy_path + train_data
-dev_path = spacy_path + dev_data
+    labels_cmd = "python -m spacy init labels config.cfg labels {} {} {} {}".format(verbose, train_path_changer, dev_path_changer, gpu_cmd)
+    os.system(labels_cmd)
 
-train_path_changer = "--paths.train "+data_path
+    train_cmd = "python -m spacy train config.cfg --output models/{} {} {} {}".format(model_name, train_path_changer, dev_path_changer, gpu_cmd)
 
-dev_path_changer = "--paths.dev "+dev_path
+    #train the thingy
+    if train:
+        os.system(train_cmd)
 
-print("training data: " ,train_path_changer)
-print("evaluation data: " ,dev_path_changer)
-verbose = "--verbose"
-
-
-
-labels_cmd = "python -m spacy init labels config.cfg labels {} {} {}".format(verbose, train_path_changer, dev_path_changer)
-os.system(labels_cmd)
-
-train_cmd = "python -m spacy train config.cfg --output models {} {}".format(train_path_changer, dev_path_changer)
-
-#train the thingy
-os.system(train_cmd)
+run("par_harem1", gpu = False, train = True)
